b9258354c0736e2c6405a13fffca445549807413
hfm-an <hfm_an@163.com>
2018-10-13 09:09:47 +0800

    add note

---
+++ javascript高级程序设计/2/defer/index.js
@@ -0,0 +1,2 @@
+console.log('这里是 defer 的 script1')
+debugger
---
+++ javascript高级程序设计/2/defer/index2.js
@@ -0,0 +1,7 @@
+/**
+ * @Author : Amnhh
+ * @Date : 2018/10/13
+ * @Email : amnhhlod@163.com
+ * @Description :
+ */
+console.log('这里是 defer 的 script2')
